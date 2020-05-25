236c80d7ce24824a869849c1afd0d389837af98c
Morten Trab <morten@trab.dk>
2020-05-25 16:32:48 +0200

    Fixed wrong cast of integer (#33)

--- custom_components/landroid_cloud/__init__.py
+++ custom_components/landroid_cloud/__init__.py
@@ -57,11 +57,11 @@ API_WORX_SENSORS = {
         },
         "icon": "mdi:battery",
         "unit": "%",
-        "device_class": None,
+        "device_class": "battery",
     },
     "error": {
         "state": {"error_description": "state", "error": "error_id"},
-        "icon": None,
+        "icon": "mdi:alert",
         "unit": None,
         "device_class": None,
     },
@@ -186,13 +186,13 @@ async def async_setup(hass, config):
                     id += 1
 
         if "raindelay" in call.data:
-            tmpdata["rd"] = call.data["raindelay"]
+            tmpdata["rd"] = int(call.data["raindelay"])
             _LOGGER.debug("Setting rain_delay for %s to %s", client[id].name, call.data["raindelay"])
             sendData = True
 
         if "timeextension" in call.data:
             tmpdata["sc"] = {} 
-            tmpdata["sc"]["p"] = call.data["timeextension"]
+            tmpdata["sc"]["p"] = int(call.data["timeextension"])
             data = json.dumps(tmpdata)
             _LOGGER.debug("Setting time_extension for %s to %s", client[id].name, call.data["timeextension"])
             sendData = True
