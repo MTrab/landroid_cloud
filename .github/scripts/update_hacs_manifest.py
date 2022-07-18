450bb7d4e5871925a7afd734f1c208617d47dd8f
Malene <malene@trab.dk>
2022-07-18 15:54:48 +0000

    Update script

--- .github/scripts/update_hacs_manifest.py
+++ .github/scripts/update_hacs_manifest.py
@@ -12,9 +12,9 @@ def update_manifest():
 
     for index, value in enumerate(sys.argv):
         if value in ["--version", "-V"]:
-            version = sys.argv[index + 1]
+            version = str(sys.argv[index + 1]).replace("v", "")
         if value in ["--path", "-P"]:
-            manifest_path = sys.argv[index + 1]
+            manifest_path = str(sys.argv[index + 1]).replace("/", "")
         if value in ["--requirements", "-R"]:
             dorequirements = True
 
@@ -22,7 +22,7 @@ def update_manifest():
         sys.exit("Missing path to manifest file")
 
     with open(
-        f"{os.getcwd()}{manifest_path}manifest.json",
+        f"{os.getcwd()}/{manifest_path}/manifest.json",
         encoding="UTF-8",
     ) as manifestfile:
         manifest = json.load(manifestfile)
@@ -52,7 +52,7 @@ def update_manifest():
             manifest["requirements"] = new_requirements
 
     with open(
-        f"{os.getcwd()}/{manifest_path.removesuffix('/').removesuffix('/')}/manifest.json",
+        f"{os.getcwd()}/{manifest_path}/manifest.json",
         "w",
         encoding="UTF-8",
     ) as manifestfile:
