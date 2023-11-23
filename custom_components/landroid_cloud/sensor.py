cc5b2398d14df90dbc9d4c39d9b0a7da88f7ae23
Malene Trab <malene@trab.dk>
2023-11-23 15:31:50 +0000

    Use correct attribute

--- custom_components/landroid_cloud/sensor.py
+++ custom_components/landroid_cloud/sensor.py
@@ -238,7 +238,7 @@ SENSORS = [
         device_class=SensorDeviceClass.TIMESTAMP,
         entity_registry_enabled_default=True,
         native_unit_of_measurement=None,
-        value_fn=lambda landroid: landroid.update.astimezone(pytz.utc),
+        value_fn=lambda landroid: landroid.updated.astimezone(pytz.utc),
         icon="mdi:clock-check",
     ),
     LandroidSensorEntityDescription(
