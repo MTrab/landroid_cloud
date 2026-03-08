f2ee9a1f68bdf8924a13bac688d444ddcd0df9eb
Malene Trab <malene@trab.dk>
2026-03-08 19:35:22 +0100

    Add legacy startup banner logging

--- custom_components/landroid_cloud/const.py
+++ custom_components/landroid_cloud/const.py
@@ -10,6 +10,17 @@ DOMAIN = "landroid_cloud"
 
 PLATFORMS: list[Platform] = [Platform.LAWN_MOWER, Platform.SENSOR]
 
+STARTUP = """
+-------------------------------------------------------------------
+Landroid Cloud integration
+
+Version: %s
+This is a custom integration
+If you have any issues with this you need to open an issue here:
+https://github.com/mtrab/landroid_cloud/issues
+-------------------------------------------------------------------
+"""
+
 CONF_CLOUD = "cloud"
 CONF_COMMAND_TIMEOUT = "command_timeout"
 
