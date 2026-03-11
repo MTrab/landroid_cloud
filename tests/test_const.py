0fbf7d8941d6253321423bafa81526d2b1125402
Malene Trab <malene@trab.dk>
2026-03-11 17:24:42 +0100

    test: update platform expectations after master merge

--- tests/test_const.py
+++ tests/test_const.py
@@ -6,9 +6,10 @@ from custom_components.landroid_cloud.const import PLATFORMS
 
 
 def test_platforms_use_home_assistant_platform_enum() -> None:
-    """Binary sensor branch should expose mower + sensor + binary sensor."""
+    """Merged branch should expose mower, sensor, switch, and binary sensor."""
     assert PLATFORMS == [
         Platform.LAWN_MOWER,
         Platform.SENSOR,
+        Platform.SWITCH,
         Platform.BINARY_SENSOR,
     ]
